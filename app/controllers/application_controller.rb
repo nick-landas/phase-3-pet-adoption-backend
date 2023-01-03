e93b429ac1c3cacf31f1ca57eb788dc467099065
Nick Landas <nick.landas@gmail.com>
2023-01-03 15:42:08 -0600

    deleted views folder and files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  configure do
-    set :views, 'app/views'
-  end
-  
-  # Agdd routes
   get '/' do
-    erb :welcome
   end
 end
