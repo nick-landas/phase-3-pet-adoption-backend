8b73aad2dcde8c748d92bcde05264ba533a69a18
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-04 19:45:52 -0600

    fix code styling to line up with ruby style guide

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
+# router for pets table using sinatra
 class PetsController < ApplicationController
+  get '/pets' do
+    Pet.all.to_json
+  end
 
-    get '/pets' do
-        pets = Pet.all.to_json
-    end
+  get '/pets/:id' do
+    Pet.find(params[:id]).to_json
+  end
 
-    get '/pets/:id' do
-        pet = Pet.find(params[:id]).to_json
-    end
+  post '/pets' do
+    Pet.create(params).to_json
+  end
 
-    post '/pets' do
-        pet = Pet.create(params).to_json
-    end
+  patch '/pets/:id' do
+    pet = pet.find(params[:id])
+    pet.update(params)
+    pet.to_json
+  end
 
-    patch '/pets/:id' do
-        pet = pet.find(params[:id])
-        pet.update(params)
-        pet.to_json
-    end
-
-    delete '/pets/:id' do
-        pet = Pet.find(params[:id]).destroy
-    end
-
-
+  delete '/pets/:id' do
+    Pet.find(params[:id]).destroy
+  end
 end
