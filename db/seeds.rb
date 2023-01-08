426440e9993ca303933eb852c9745c9bb1e4ef29
Nick Landas <nick.landas@gmail.com>
2023-01-07 21:03:01 -0600

    Changed seeding parameters to pull 100 pets for testing of pagination functionality.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@ puts "🌱Seeding..."
 AdoptionApplication.destroy_all
 Owner.destroy_all
 Pet.destroy_all
-
-10.times do
+Faker::UniqueGenerator.clear
+100.times do
   species = Faker::Creature::Animal.name
   Pet.create!(
     species: species,
     color: Faker::Color.color_name,
     weight: Faker::Number.between(from: 1, to: 100),
     house_broken: Faker::Boolean.boolean,
-    name: Faker::Name.unique.neutral_first_name,
+    name: Faker::Name.neutral_first_name,
     personality: Faker::Adjective.positive,
-    image: Faker::LoremFlickr.unique.image(size: '320x300', search_terms: [species.to_s, 'animal'], match_all: true)
+    image: Faker::LoremFlickr.image(size: '320x300', search_terms: [species.to_s, 'animal'], match_all: true)
   )
 end
 
